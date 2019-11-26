8812c15d15b0ad4d8ecb840109089a2b2834b4fe
Victor Landa <victorlanda.dev@gmail.com>
2019-11-26 00:31:27 -0600

    Initial

--- src/components/Buttom.jsx
+++ src/components/Buttom.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Componente Statefull
 class Button extends React.Component {
 	state = {
 		count: 0,
@@ -15,7 +16,7 @@ class Button extends React.Component {
 		const { count } = this.state;
 		return(
 			<div>
-				<h1>Libros: {count}</h1>
+				<h1>Libros 📚: {count}</h1>
 				<button type="button" onClick={this.handleClick}>Add a book to your cart!</button>
 			</div>
 		)
